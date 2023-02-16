687fd0a1c38782c0218413a25a4eceae7b259eae
Lin Jian <me@linj.tech>
2023-02-16 20:58:06 +0800

    kanata: add a patch for systemd notify

--- pkgs/tools/system/kanata/default.nix
+++ pkgs/tools/system/kanata/default.nix
@@ -16,13 +16,18 @@ rustPlatform.buildRustPackage rec {
     sha256 = "sha256-mQSbsJ+3mKoDMg0ewwR7UvXUq+5WA9aTPKWCaTz8nDE=";
   };
 
-  cargoHash = "sha256-Pu96OGfnXNaIse/IcwFJWxGMlKOVhZ6DtvgXJkHh+Ao=";
+  cargoHash = "sha256-iLd30aLEIJLYdtsh10KOcoi2+yXZc07Oh9yW1g3EvEo=";
 
   cargoPatches = [
     (fetchpatch {
       name = "serialize-cfg-parsing-tests-for-1.2.0.patch";
       url = "https://github.com/jtroo/kanata/commit/9ef1e80fbcb40402262e08bd9196d000f73f686d.patch";
       hash = "sha256-/FhyaYx4usDjGoVfRktf9dtwjY4oXdMQKqxLz00/NPY=";
+    })
+    (fetchpatch {
+      name = "notify-systemd-when-kanata-has-finished-starting-up-for-1.2.0.patch";
+      url = "https://github.com/jtroo/kanata/commit/c46e0ec88344e4adda5f7cf35fea288eb0d2203a.patch";
+      hash = "sha256-ZR9iwr/V9Vplp1+Ubdpls8C0AAI0V5uebx2BibpdxoE=";
     })
   ];
 
