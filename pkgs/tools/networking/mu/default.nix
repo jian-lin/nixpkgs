3e62282db3eb0fe0a3254550d2bc60bfeaf0e844
Charlotte Van Petegem <charlotte@vanpetegem.me>
2023-03-26 12:41:08 +0200

    mu: 1.18.14 -> 1.10.2
    https://github.com/djcb/mu/releases/tag/v1.10.0
    https://github.com/djcb/mu/releases/tag/v1.10.1
    https://github.com/djcb/mu/releases/tag/v1.10.2

--- pkgs/tools/networking/mu/default.nix
+++ pkgs/tools/networking/mu/default.nix
@@ -14,13 +14,13 @@
 
 stdenv.mkDerivation rec {
   pname = "mu";
-  version = "1.8.14";
+  version = "1.10.2";
 
   src = fetchFromGitHub {
     owner = "djcb";
     repo = "mu";
     rev = "v${version}";
-    hash = "sha256-m6if0Br9WRPR8POwOM0Iwido3UR/V0BlkuaLcWsf/c0=";
+    hash = "sha256-BJ0RTTF8ZnYOGGYmN0qj+cZsbKHnV4x6Job/JYbp0A0=";
   };
 
   postPatch = ''
